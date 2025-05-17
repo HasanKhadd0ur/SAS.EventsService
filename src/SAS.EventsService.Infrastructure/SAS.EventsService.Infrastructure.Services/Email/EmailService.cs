d21ac245ace788f17262958c2107371b07da4385
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-05-17 14:35:22 +0300

    Add Dummy Email Service

---
+++ src/SAS.EventsService.Infrastructure/SAS.EventsService.Infrastructure.Services/Email/EmailService.cs
@@ -0,0 +1,21 @@
+﻿using SAS.EventsService.Application.Contracts.Email;
+
+namespace SAS.EventsService.Infrastructure.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendBulkEmailsAsync(IEnumerable<string> recipient, string subject, string body)
+        {
+            Console.WriteLine("a messages sended " + "|| body is " + body + "|| subject is" + subject);
+            return Task.CompletedTask;
+
+        }
+
+        public Task SendEmailAsync(string recipient, string subject, string body)
+        {
+            Console.WriteLine("a message sended to " + recipient + "|| body is " + body + "|| subject is" + subject);
+            return Task.CompletedTask;
+
+        }
+    }
+}
