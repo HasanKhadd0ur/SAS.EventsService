a735ec8bff2b78831697ea73fce6f90979a2a4aa
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-12 10:41:01 +0300

    Add Name Entity to event usecase

---
+++ src/SAS.EventsService.Application/Events/UseCases/Commands/AddNamedEntityToEvent/AddNamedEntityToEventCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using Ardalis.Result;
+using SAS.EventsService.Application.Contracts.Providers;
+using SAS.EventsService.Domain.Common.Errors;
+using SAS.EventsService.Domain.Events.Repositories;
+using SAS.EventsService.SharedKernel.CQRS.Commands;
+using SAS.EventsService.SharedKernel.Utilities;
+
+namespace SAS.EventsService.Application.Events.UseCases.Commands.AddNamedEntityToEvent
+{
+    public class AddNamedEntityToEventCommandHandler : ICommandHandler<AddNamedEntityToEventCommand, Result>
+    {
+        private readonly IEventsRepository _eventsRepository;
+        private readonly INamedEntitiesRepository _namedEntitiesRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IDateTimeProvider _dateTimeProvider;
+
+        public AddNamedEntityToEventCommandHandler(
+            IEventsRepository eventsRepository,
+            INamedEntitiesRepository namedEntitiesRepository,
+            IUnitOfWork unitOfWork,
+            IDateTimeProvider dateTimeProvider)
+        {
+            _eventsRepository = eventsRepository;
+            _namedEntitiesRepository = namedEntitiesRepository;
+            _unitOfWork = unitOfWork;
+            _dateTimeProvider = dateTimeProvider;
+        }
+
+        public async Task<Result> Handle(AddNamedEntityToEventCommand request, CancellationToken cancellationToken)
+        {
+            var @event = await _eventsRepository.GetByIdAsync(request.EventId);
+            if (@event is null)
+                return Result.Invalid(EventErrors.UnExistEvent);
+
+            var namedEntity = await _namedEntitiesRepository.GetByIdAsync(request.NamedEntityId);
+            if (namedEntity is null)
+                return Result.Invalid(NamedEntityErrors.UnExistNamedEntity);
+
+            @event.AddNamedEntityMention(namedEntity);
+            @event.UpdateLastModifiedTime(_dateTimeProvider.UtcNow);
+
+            await _eventsRepository.UpdateAsync(@event);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Success();
+        }
+    }
+}
