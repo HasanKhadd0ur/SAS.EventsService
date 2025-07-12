9514a506577510a9a5008082f79f99bff48e5f35
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-12 10:43:21 +0300

    Add GetAll, Delete Named Entity Use Case

---
+++ src/SAS.EventsService.Application/NamedEntities/UseCases/Commands/DeleteNamedEntity/DeleteNamedEntityCommand.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Result;
+using SAS.EventsService.SharedKernel.CQRS.Commands;
+
+namespace SAS.EventsService.Application.NamedEntities.UseCases.Commands.DeleteNamedEntity
+{
+    public record DeleteNamedEntityCommand(Guid Id) : ICommand<Result>;
+
+}
