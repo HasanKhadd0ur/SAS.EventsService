fd04c0688a87836d75fc7ca5cd4ef513f9ffd6ce
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-07-12 10:40:46 +0300

    Add Named Entity use cases

---
+++ src/SAS.EventsService.Application/NamedEntities/Common/NamedEntityTypeDto.cs
@@ -0,0 +1,10 @@
+﻿using SAS.EventsService.Application.Common;
+
+namespace SAS.EventsService.Application.NamedEntities.Common
+{
+    public class NamedEntityTypeDto : BaseDTO<Guid>
+    {
+        public string TypeName { get; set; }
+        public string NormalisedName { get; set; }
+    }
+}
